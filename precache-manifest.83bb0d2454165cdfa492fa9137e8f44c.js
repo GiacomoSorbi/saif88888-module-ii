7d773e0e462362af6e8d0e8991c46293f84c65f6
saif88888 <saif_saeed_cs@yahoo.com>
2020-07-16 21:23:21 +0100

    Updates

---
+++ precache-manifest.83bb0d2454165cdfa492fa9137e8f44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2654924559c8a9ade773c1ee86ed53d3",
+    "url": "/saif88888-module-ii/index.html"
+  },
+  {
+    "revision": "5c517e85919456021e23",
+    "url": "/saif88888-module-ii/static/css/main.0df4294a.chunk.css"
+  },
+  {
+    "revision": "b6391bbfd279a265c25a",
+    "url": "/saif88888-module-ii/static/js/2.ec86b53c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saif88888-module-ii/static/js/2.ec86b53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c517e85919456021e23",
+    "url": "/saif88888-module-ii/static/js/main.e75633e0.chunk.js"
+  },
+  {
+    "revision": "e2c9b8f0641da8d52d39",
+    "url": "/saif88888-module-ii/static/js/runtime-main.bca79e1a.js"
+  }
+]);
