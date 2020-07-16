2b6caeae70172894dda653f9e65503c2bcc7bf62
saif88888 <saif_saeed_cs@yahoo.com>
2020-07-16 22:14:39 +0100

    update 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-import Navbar from "./Components/Navbar";
+
 import Contact from "./Components/Contact";
 import Profile from "./Components/Profile";
 import Work from "./Components/Work";
 import Index from "./Components/Index";
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Navbar />
+      <Switch>
         <Route exact path="/" component={Index} />
         <Route path="/Profile" component={Profile} />
         <Route path="/Work" component={Work} />
         <Route path="/contact" component={Contact} />
-      </BrowserRouter>
+      </Switch>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
+      <Navbar />
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
